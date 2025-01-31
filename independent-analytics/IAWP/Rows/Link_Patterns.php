e45938247a59cb5e6ba876a6618ae0e7b90513d4
Justin Gardner <justin@rhynorater.com>
2025-01-31 06:03:24 -0500

    2.10.0

--- independent-analytics/IAWP/Rows/Link_Patterns.php
+++ independent-analytics/IAWP/Rows/Link_Patterns.php
@@ -8,13 +8,12 @@ use IAWP\Query_Taps;
 use IAWPSCOPED\Illuminate\Database\Query\Builder;
 use IAWPSCOPED\Illuminate\Database\Query\JoinClause;
 /** @internal */
-class Clicks extends \IAWP\Rows\Rows
+class Link_Patterns extends \IAWP\Rows\Rows
 {
     public function attach_filters(Builder $query) : void
     {
         $query->joinSub($this->query(\true), 'click_rows', function (JoinClause $join) {
             $join->on('click_rows.link_rule_id', '=', 'link_rules.link_rule_id');
-            $join->on('click_rows.click_target_id', '=', 'click_targets.click_target_id');
         });
     }
     protected function fetch_rows() : array
@@ -29,7 +28,7 @@ class Clicks extends \IAWP\Rows\Rows
         if ($skip_pagination) {
             $this->number_of_rows = null;
         }
-        $query = Illuminate_Builder::new()->select(['link_rules.link_rule_id AS link_rule_id', 'click_targets.click_target_id AS click_target_id', 'link_rules.name AS link_name', 'click_targets.target AS link_target'])->selectRaw('COUNT(DISTINCT clicks.click_id) AS link_clicks')->from($this->tables::link_rules(), 'link_rules')->leftJoin($this->tables::clicked_links() . ' AS clicked_links', 'clicked_links.link_rule_id', '=', 'link_rules.link_rule_id')->leftJoin($this->tables::clicks() . ' AS clicks', 'clicks.click_id', '=', 'clicked_links.click_id')->leftJoin($this->tables::click_targets() . ' AS click_targets', 'click_targets.click_target_id', '=', 'clicks.click_target_id')->whereBetween('clicks.created_at', $this->get_current_period_iso_range())->when(\count($this->filters) > 0, function (Builder $query) {
+        $query = Illuminate_Builder::new()->select(['link_rules.link_rule_id AS link_rule_id', 'link_rules.name AS link_name'])->selectRaw('COUNT(DISTINCT clicks.click_id) AS link_clicks')->from($this->tables::link_rules(), 'link_rules')->leftJoin($this->tables::clicked_links() . ' AS clicked_links', 'clicked_links.link_rule_id', '=', 'link_rules.link_rule_id')->leftJoin($this->tables::clicks() . ' AS clicks', 'clicks.click_id', '=', 'clicked_links.click_id')->leftJoin($this->tables::click_targets() . ' AS click_targets', 'click_targets.click_target_id', '=', 'clicks.click_target_id')->whereBetween('clicks.created_at', $this->get_current_period_iso_range())->when(\count($this->filters) > 0, function (Builder $query) {
             foreach ($this->filters as $filter) {
                 if (!$this->is_a_calculated_column($filter->column())) {
                     $filter->apply_to_query($query);
@@ -37,7 +36,7 @@ class Clicks extends \IAWP\Rows\Rows
             }
         })->when(\is_int($this->number_of_rows), function (Builder $query) {
             $query->limit($this->number_of_rows);
-        })->tap(Query_Taps::tap_authored_content_for_clicks())->orderBy($this->sort_configuration->column(), $this->sort_configuration->direction())->orderBy('link_name')->groupBy('link_rules.link_rule_id', 'clicks.click_target_id');
+        })->tap(Query_Taps::tap_authored_content_for_clicks())->orderBy($this->sort_configuration->column(), $this->sort_configuration->direction())->orderBy('link_name')->groupBy('link_rules.link_rule_id');
         return $query;
     }
 }
--- independent-analytics/IAWP/Views_Over_Time_Finder.php
+++ independent-analytics/IAWP/Views_Over_Time_Finder.php
@@ -5,7 +5,7 @@ namespace IAWP;
 use IAWP\Date_Range\Date_Range;
 use IAWP\Interval\Interval;
 /** @internal */
-class Visitors_Over_Time_Finder
+class Views_Over_Time_Finder
 {
     /**
      * @var Date_Range
@@ -30,27 +30,26 @@ class Visitors_Over_Time_Finder
         $date_interval = $this->interval->get_date_interval();
         $date_period = new \DatePeriod($this->date_range->start(), $date_interval, $this->date_range->end());
         $interval_data = [];
-        $visitors_data = [];
         $views_data = [];
         foreach ($date_period as $index => $date) {
             $current_interval = $index;
-            $current_visitors = 0;
             $current_views = 0;
             foreach ($rows as $row) {
                 $row_interval = \intval($row->interval_ago);
-                $row_visitors = \intval($row->visitors);
                 $row_views = \intval($row->views);
                 if ($row_interval === $index) {
                     $current_interval = $row_interval;
-                    $current_visitors = $row_visitors;
                     $current_views = $row_views;
                     break;
                 }
             }
             $interval_data[] = $current_interval;
-            $visitors_data[] = $current_visitors;
             $views_data[] = $current_views;
         }
-        return (object) ['visitors' => $visitors_data, 'views' => $views_data, 'interval_labels_short' => $this->interval->get_short_labels($interval_data), 'interval_labels_full' => $this->interval->get_full_labels($interval_data)];
+        // Reverse the data so the most recent data comes last. The end result is that the real-time
+        // chart has the most recent data on the right with older data on the left.
+        $interval_data = \array_reverse($interval_data);
+        $views_data = \array_reverse($views_data);
+        return (object) ['views' => $views_data, 'interval_labels_short' => $this->interval->get_short_labels($interval_data), 'interval_labels_full' => $this->interval->get_full_labels($interval_data)];
     }
 }
